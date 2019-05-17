f11d20d44bad1e17e0abd07fdd51ba332716f5af
pete-restall <pete@restall.net>
2019-05-17 17:29:09 +0100

    Added some power-saving TODOs.

--- src/firmware/src/Lcd/LcdEnableDisable.c
+++ src/firmware/src/Lcd/LcdEnableDisable.c
@@ -18,7 +18,23 @@ void onVoltageRegulatorEnabled(const struct Event *event)
 {
 	if (lcdState.enableCount != 0)
 		lcdConfigure();
+
+	// TRISAbits.TRISA2 = 1; // TODO: NEEDS TESTING.  SAVES 6mA THOUGH !
 }
+
+// TODO: NEEDS TESTING.  SAVES 6mA THOUGH !
+// TODO: THE IDEA IS TO TRI-STATE THE CONTRAST PIN WHEN THE VOLTAGE REGULATOR
+// IS ENABLED BUT THE LCD IS NOT IN USE, THUS SAVING 6mA FROM NOT SWITCHING
+// THE CONTRAST PWM.  THE PIN MUST BE AN OUTPUT WHEN THE VOLTAGE REGULATOR IS
+// DISABLED, HOWEVER, SINCE OTHERWISE IT WOULD FLOAT (THE LATCH IS INITIALISED
+// TO ZERO AND SHOULD NOT BE SET ANYWHERE ELSE, SO ALWAYS PULLED LOW SO WON'T
+// TRY TO SOURCE CURRENT THROUGH THE LCD'S ESD DIODES)
+//
+//void onVoltageRegulatorDisabledOrLcdEnabled(const struct Event *event)
+//{
+//	THESE EVENTS NEED TO BE SUBSCRIBED TO, PROBABLY IN THE LCD INITIALISE.
+//	TRISAbits.TRISA2 = 0;
+//}
 
 void lcdDisable(void)
 {
@@ -27,6 +43,7 @@ void lcdDisable(void)
 
 	if (--lcdState.enableCount == 0)
 		lcdState.flags.busy = 1;
+		// TRISAbits.TRISA2 = 1; // TODO: NEEDS TESTING.  SAVES 6mA THOUGH !
 
 	pwmTimerDisable();
 	voltageRegulatorDisable();
