bb6cdad5f1b274756cbd52b4cc27e9120151ac5e
pete-restall <pete@restall.net>
2019-06-19 17:32:26 +0100

    Using no progress indicators to reduce the verbosity of the build logs.

--- src/firmware/build/install-mplabx.sh
+++ src/firmware/build/install-mplabx.sh
@@ -8,10 +8,10 @@ MPLABX_INSTALLER="MPLABX-${MPLABX_VERSION}-linux-installer.sh";
 
 XC8_INSTALLER="xc8-${XC8_VERSION}-full-install-linux-installer.run";
 
-wget --progress=bar "http://ww1.microchip.com/downloads/en/DeviceDoc/${MPLABX_INSTALLER_TAR}";
+wget --quiet "http://ww1.microchip.com/downloads/en/DeviceDoc/${MPLABX_INSTALLER_TAR}";
 tar -xvf "${MPLABX_INSTALLER_TAR}";
 sudo "./${MPLABX_INSTALLER}" -- --mode unattended;
 
-wget --progress=bar "http://ww1.microchip.com/downloads/en/DeviceDoc/${XC8_INSTALLER}";
+wget --quiet "http://ww1.microchip.com/downloads/en/DeviceDoc/${XC8_INSTALLER}";
 chmod +x "${XC8_INSTALLER}";
 sudo "./${XC8_INSTALLER}" --mode unattended --netservername whatever;
