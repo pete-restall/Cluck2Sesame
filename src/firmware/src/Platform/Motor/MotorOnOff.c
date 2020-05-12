5398fa4a4bea357dabed9929c023c8583dd599a3
pete-restall <pete@restall.net>
2020-05-12 14:32:39 +0100

    A whole host of motor changes based off bench testing and tweaking.  This has broken a lot of the tests because of the complexity of dealing with a non-idealised real-world - will figure out what to do with them later.

--- src/firmware/src/Platform/Motor/MotorOnOff.c
+++ src/firmware/src/Platform/Motor/MotorOnOff.c
@@ -1,3 +1,7 @@
+/* NOTE: The ceedling tests do not cover a lot of this file as it is complex due to the asynchronous nature of the motor
+   control.  If you make changes then there is no substitute for bench testing the motor as it is quicker and simpler
+   and guaranteed to reflect the real world. */
+
 #include <xc.h>
 #include <stdint.h>
 
@@ -6,28 +10,45 @@
 
 #include "Motor.h"
 
+#define PWM_INCREMENT_TICK_COUNT 10
+#define ENCODER_STATIC_COUNT_LIMIT 10
+
 #define CCP1CON_MODE_MASK (0b1111 << _CCP1CON_MODE_POSITION)
 #define CCP1CON_COMPARE_AND_SET_MODE (0b1000 << _CCP1CON_MODE_POSITION)
 
-static void incrementPwmDutyCycle(void *state);
+#define MOTOR_RUNNING_STATE_STOPPED 0
+#define MOTOR_RUNNING_STATE_STARTING 1
+#define MOTOR_RUNNING_STATE_STARTED 2
 
+static void incrementPwmDutyCycle(void *state);
+static void motorRunningStateMonitor(void *state);
+
+static struct MotorStopped motorStoppedEventArgs;
+static uint8_t motorRunningState;
 static struct MotorStarted motorStartedEventArgs;
 
 static const struct NearSchedule pwmDutyCycleIncrementingSchedule =
 {
-	.ticks = 10, // TODO: TESTS WILL NEED UPDATING FOR THIS...(WAS 1, NOW 10)
+	.ticks = PWM_INCREMENT_TICK_COUNT,
 	.handler = &incrementPwmDutyCycle
+};
+
+static const struct NearSchedule motorRunningStateMonitorSchedule =
+{
+	.ticks = 32 * PWM_INCREMENT_TICK_COUNT / ENCODER_STATIC_COUNT_LIMIT,
+	.handler = &motorRunningStateMonitor
 };
 
 void motorOn(int16_t count)
 {
-	if (count == 0 || (CWG1STR & 0x0f))
+	if (count == 0 || (CWG1STR & 0x0f) || motorRunningState != MOTOR_RUNNING_STATE_STOPPED)
 		return;
 
+	motorRunningState = MOTOR_RUNNING_STATE_STARTING;
 	motorStartedEventArgs.count = count;
 	eventPublish(MOTOR_STARTED, &motorStartedEventArgs);
 
-	uint8_t steeringMask = _CWG1STR_STRA_MASK; // TODO: A AND B HAVE BEEN SWITCHED AROUND AS THEY WERE WRONG...FIX THE TESTS...
+	uint8_t steeringMask = _CWG1STR_STRA_MASK;
 	if (count < 0)
 	{
 		steeringMask = _CWG1STR_STRB_MASK;
@@ -52,17 +73,18 @@ void motorOn(int16_t count)
 	   that alludes to this. */
 
 	PIR4bits.TMR1IF = 0;
-	PIR2bits.C1IF = 0; // TODO: SHOULD REALLY CLEAR THIS HERE - COMPARATOR CAN BE TWITCHY...
-	PIR6bits.CCP1IF = 0; // TODO: NEEDS ADDING TO THE TESTS
-	PIR5bits.CLC2IF = 0; // TODO: NEEDS ADDING TO THE TESTS
-	CWG1AS0bits.SHUTDOWN = 0; // TODO: NEEDS ADDING TO THE TESTS
-	PIR7bits.CWG1IF = 0; // TODO: NEEDS ADDING TO THE TESTS
+	PIR2bits.C1IF = 0;
+	PIR6bits.CCP1IF = 0;
+	PIR5bits.CLC2IF = 0;
+	CWG1AS0bits.SHUTDOWN = 0;
+	PIR7bits.CWG1IF = 0;
 
 	/* ******************************************************************* */
 
 	CWG1STR |= steeringMask;
 
-	nearSchedulerAddOrUpdate(&pwmDutyCycleIncrementingSchedule); // TODO: TESTS WILL NEED UPDATING FOR THIS...CHANGED FROM 'add' TO 'add or update'
+	nearSchedulerAddOrUpdate(&pwmDutyCycleIncrementingSchedule);
+	nearSchedulerAddOrUpdate(&motorRunningStateMonitorSchedule);
 }
 
 static void incrementPwmDutyCycle(void *state)
@@ -71,7 +93,7 @@ static void incrementPwmDutyCycle(void *state)
 		return;
 
 	if (PWM4DCH != (256 - 8) >> 2)
-		nearSchedulerAddOrUpdate(&pwmDutyCycleIncrementingSchedule); // TODO: TESTS WILL NEED UPDATING FOR THIS...CHANGED FROM 'add' TO 'add or update'
+		nearSchedulerAddOrUpdate(&pwmDutyCycleIncrementingSchedule);
 
 	PWM4DCH += 8 >> 2;
 }
@@ -80,23 +102,56 @@ void motorOff(void)
 {
 	if (CWG1STR & CWG1STR_STEERING_MASK)
 	{
-		uint8_t antiClockwise = CWG1STRbits.STRB; // TODO: A AND B HAVE BEEN SWITCHED AROUND AS THEY WERE WRONG...FIX THE TESTS...
 		CWG1STR &= ~CWG1STR_STEERING_MASK;
-
-		static struct MotorStopped eventArgs;
-		eventArgs.fault.currentLimited = PIR2bits.C1IF != 0 ? 1 : 0;
-		eventArgs.fault.encoderOverflow = PIR4bits.TMR1IF != 0 ? 1 : 0;
-		// TODO: eventArgs.fault.encoderTimeout = ???
-		eventArgs.requestedCount = motorStartedEventArgs.count;
-		eventArgs.actualCount = (int16_t) ((uint16_t) TMR1L);
-		eventArgs.actualCount |= ((int16_t) TMR1H) << 8;
-		if (antiClockwise)
-			eventArgs.actualCount = -eventArgs.actualCount;
-
-		eventPublish(MOTOR_STOPPED, &eventArgs);
+		motorStoppedEventArgs.requestedCount = motorStartedEventArgs.count;
+		motorStoppedEventArgs.fault.all = 0;
+		motorStoppedEventArgs.fault.currentLimited = PIR2bits.C1IF != 0 ? 1 : 0;
+		motorStoppedEventArgs.fault.encoderOverflow = PIR4bits.TMR1IF != 0 ? 1 : 0;
 	}
 
 	PIR2bits.C1IF = 0;
 	PIR4bits.TMR1IF = 0;
 	PIR7bits.CWG1IF = 0;
+}
+
+static void motorRunningStateMonitor(void *state)
+{
+	static uint16_t previousEncoderValue;
+	static uint8_t encoderStaticCount;
+
+	uint16_t encoderValue = (uint16_t) TMR1L;
+	encoderValue |= ((uint16_t) TMR1H) << 8;
+
+	if (motorRunningState == MOTOR_RUNNING_STATE_STARTED && encoderValue == previousEncoderValue)
+	{
+		if (++encoderStaticCount == ENCODER_STATIC_COUNT_LIMIT)
+		{
+			motorStoppedEventArgs.actualCount = encoderValue;
+			if (motorStoppedEventArgs.requestedCount < 0)
+			{
+				motorStoppedEventArgs.actualCount = -motorStoppedEventArgs.actualCount;
+				if (motorStoppedEventArgs.actualCount > motorStoppedEventArgs.requestedCount && motorStoppedEventArgs.fault.all == 0)
+					motorStoppedEventArgs.fault.encoderTimeout = 1;
+			}
+			else
+			{
+				if (motorStoppedEventArgs.actualCount < motorStoppedEventArgs.requestedCount && motorStoppedEventArgs.fault.all == 0)
+					motorStoppedEventArgs.fault.encoderTimeout = 1;
+			}
+
+			eventPublish(MOTOR_STOPPED, &motorStoppedEventArgs);
+			motorRunningState = MOTOR_RUNNING_STATE_STOPPED;
+			return;
+		}
+	}
+	else
+	{
+		if (motorRunningState == MOTOR_RUNNING_STATE_STARTING)
+			motorRunningState = MOTOR_RUNNING_STATE_STARTED;
+
+		encoderStaticCount = 0;
+	}
+
+	previousEncoderValue = encoderValue;
+	nearSchedulerAddOrUpdate(&motorRunningStateMonitorSchedule);
 }
