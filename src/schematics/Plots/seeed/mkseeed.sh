2dbcbd9395d6cbcc6a80192fae963ea68a8a5341
pete-restall <pete@restall.net>
2021-11-21 20:06:49 +0000

    Replaced obsolete parts with substitutes.  Started to put together the manufacturing package for Seeed.  Renamed Cluck3Sesame to Cluck2Sesame and bumped the version number instead.

--- src/schematics/Plots/seeed/mkseeed.sh
+++ src/schematics/Plots/seeed/mkseeed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROJECT="Cluck3Sesame";
+PROJECT="Cluck2Sesame";
 PLOT_REPORT="../${PROJECT} (Plot Report).txt";
 PLOT_DIR="../";
 ZIP_DIR="${PROJECT}";
@@ -16,7 +16,7 @@ if [[ -a ${ZIP_FILE} ]]; then
 	rm ${ZIP_FILE};
 fi;
 
-cp "${PLOT_DIR}/${PROJECT} - Board Outline.gbr" "${ZIP_DIR}/Board Outline.GBL";
+cp "${PLOT_DIR}/${PROJECT} - Board Outline.gbr" "${ZIP_DIR}/Board Outline.GM1";
 cp "${PLOT_DIR}/${PROJECT} - Bottom Copper.gbr" "${ZIP_DIR}/Bottom Layer.GBL";
 cp "${PLOT_DIR}/${PROJECT} - Bottom Solder Mask.gbr" "${ZIP_DIR}/Bottom Solder Mask.GBS";
 cp "${PLOT_DIR}/${PROJECT} - Bottom Paste Mask.gbr" "${ZIP_DIR}/Bottom Paste Mask.GBP";
@@ -25,9 +25,11 @@ cp "${PLOT_DIR}/${PROJECT} - Drill Data - [Through Hole].drl" "${ZIP_DIR}/Drill 
 cp "${PLOT_DIR}/${PROJECT} - Drill Data - [Through Hole] (Unplated).drl" "${ZIP_DIR}/Drill Drawing Unplated.NC";
 cp "${PLOT_DIR}/${PROJECT} - Top Copper.gbr" "${ZIP_DIR}/Top Layer.GTL";
 cp "${PLOT_DIR}/${PROJECT} - Top Solder Mask.gbr" "${ZIP_DIR}/Top Solder Mask.GTS";
-cp "${PLOT_DIR}/${PROJECT} - Top Paste Mask.gbr" "${ZIP_DIR}/Top Paste Mask.GTP";
 cp "${PLOT_DIR}/${PROJECT} - Top Silkscreen.gbr" "${ZIP_DIR}/Top Overlay.GTO";
 cp "README.txt" "${ZIP_DIR}/README.TXT";
 cp "${PLOT_REPORT}" "${ZIP_DIR}/DesignSpark Plot Report.TXT";
 
 zip -9 -r "${ZIP_FILE}" "${ZIP_DIR}";
+
+echo;
+echo "*** NOTE: MAKE SURE UNPLATED HOLES ARE CORRECT (PROBABLY EMPTY)";
