b88be93c8f057806495a86ca76a287465bdff33b
pete-restall <pete@restall.net>
2021-12-06 19:13:41 +0000

    Updated the XC8 compiler to see if the cromwell bugfix for Ubuntu is included.

--- src/firmware/build/install-mplabx.sh
+++ src/firmware/build/install-mplabx.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 MPLABX_VERSION="v5.20";
-XC8_VERSION="v2.05";
+XC8_VERSION="v2.32";
 
 MPLABX_INSTALLER_TAR="MPLABX-${MPLABX_VERSION}-linux-installer.tar";
 MPLABX_INSTALLER="MPLABX-${MPLABX_VERSION}-linux-installer.sh";
 
-XC8_INSTALLER="xc8-${XC8_VERSION}-full-install-linux-installer.run";
+XC8_INSTALLER="xc8-${XC8_VERSION}-full-install-linux-x64-installer.run";
 
-wget --quiet "http://ww1.microchip.com/downloads/en/DeviceDoc/${MPLABX_INSTALLER_TAR}";
+wget --quiet "https://ww1.microchip.com/downloads/en/DeviceDoc/${MPLABX_INSTALLER_TAR}";
 tar -xvf "${MPLABX_INSTALLER_TAR}";
 sudo "./${MPLABX_INSTALLER}" -- --mode unattended;
 
-wget --quiet "http://ww1.microchip.com/downloads/en/DeviceDoc/${XC8_INSTALLER}";
+wget --quiet "https://www.microchip.com/content/dam/mchp/documents/DEV/ProductDocuments/SoftwareTools/${XC8_INSTALLER}";
 chmod +x "${XC8_INSTALLER}";
 sudo "./${XC8_INSTALLER}" --mode unattended --netservername whatever;
