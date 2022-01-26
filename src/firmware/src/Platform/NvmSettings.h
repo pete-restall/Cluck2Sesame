c19e64e003bb478e815ce86f2e76c687ee93a4d5
pete-restall <pete@restall.net>
2022-01-26 08:40:47 +0000

    Removed some TODOs as they've been done.

--- src/firmware/src/Platform/NvmSettings.h
+++ src/firmware/src/Platform/NvmSettings.h
@@ -31,9 +31,9 @@ union NvmSettings
 
 				struct
 				{
-					uint16_t temperatureHighAdc; // TODO: 8x SAMPLES OF ADC AT THE HIGHEST CALIBRATION TEMPERATURE
-					uint8_t temperatureHighCelsius; // TODO: CELSIUS OF HIGHEST CALIBRATION TEMPERATURE
-					uint16_t temperatureCoefficient; // TODO: COEFFICIENT (TH - TL) / (TH_adc - TL_adc); ALWAYS POSITIVE, AROUND 1 COUNT PER CELSIUS, EVEN THOUGH SLOPE IS NEGATIVE
+					uint16_t temperatureHighAdc;
+					uint8_t temperatureHighCelsius;
+					uint16_t temperatureCoefficient;
 				} temperature;
 
 				struct
