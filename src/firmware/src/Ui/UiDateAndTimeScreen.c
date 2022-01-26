68974cd9c984ef12e571a9214060e43be1cd6187
pete-restall <pete@restall.net>
2022-01-26 08:40:32 +0000

    Some UI bugfixes for the displaying of the date and time.

--- src/firmware/src/Ui/UiDateAndTimeScreen.c
+++ src/firmware/src/Ui/UiDateAndTimeScreen.c
@@ -7,21 +7,37 @@
 
 #include "Ui.h"
 
-#define DATE_YY1_POS UI_CURSOR_AT(2, 1)
-#define DATE_YY2_POS (DATE_YY1_POS + 1)
-#define DATE_YY_MM_SEPARATOR_POS (DATE_YY2_POS + 1)
-#define DATE_MM1_POS (DATE_YY_MM_SEPARATOR_POS + 1)
-#define DATE_MM2_POS (DATE_MM1_POS + 1)
-#define DATE_MM_DD_SEPARATOR_POS (DATE_MM2_POS + 1)
-#define DATE_DD1_POS (DATE_MM_DD_SEPARATOR_POS + 1)
-#define DATE_DD2_POS (DATE_DD1_POS + 1)
-#define DATE_TIME_SEPARATOR_POS (DATE_DD2_POS + 1)
-#define TIME_HH1_POS (DATE_TIME_SEPARATOR_POS + 1)
-#define TIME_HH2_POS (TIME_HH1_POS + 1)
-#define TIME_HH_MM_SEPARATOR_POS (TIME_HH2_POS + 1)
-#define TIME_MM1_POS (TIME_HH_MM_SEPARATOR_POS + 1)
-#define TIME_MM2_POS (TIME_MM1_POS + 1)
-#define DATE_ENTRY_COMPLETED (TIME_MM2_POS + 1)
+#define ENTRY_DATE_YY1_POS UI_CURSOR_AT(2, 1)
+#define ENTRY_DATE_YY2_POS (ENTRY_DATE_YY1_POS + 1)
+#define ENTRY_DATE_YY_MM_SEPARATOR_POS (ENTRY_DATE_YY2_POS + 1)
+#define ENTRY_DATE_MM1_POS (ENTRY_DATE_YY_MM_SEPARATOR_POS + 1)
+#define ENTRY_DATE_MM2_POS (ENTRY_DATE_MM1_POS + 1)
+#define ENTRY_DATE_MM_DD_SEPARATOR_POS (ENTRY_DATE_MM2_POS + 1)
+#define ENTRY_DATE_DD1_POS (ENTRY_DATE_MM_DD_SEPARATOR_POS + 1)
+#define ENTRY_DATE_DD2_POS (ENTRY_DATE_DD1_POS + 1)
+#define ENTRY_DATE_TIME_SEPARATOR_POS (ENTRY_DATE_DD2_POS + 1)
+#define ENTRY_TIME_HH1_POS (ENTRY_DATE_TIME_SEPARATOR_POS + 1)
+#define ENTRY_TIME_HH2_POS (ENTRY_TIME_HH1_POS + 1)
+#define ENTRY_TIME_HH_MM_SEPARATOR_POS (ENTRY_TIME_HH2_POS + 1)
+#define ENTRY_TIME_MM1_POS (ENTRY_TIME_HH_MM_SEPARATOR_POS + 1)
+#define ENTRY_TIME_MM2_POS (ENTRY_TIME_MM1_POS + 1)
+#define ENTRY_DATE_ENTRY_COMPLETED (ENTRY_TIME_MM2_POS + 1)
+
+#define STATUS_DATE_YY1_POS UI_CURSOR_AT(2, 0)
+#define STATUS_DATE_YY2_POS (STATUS_DATE_YY1_POS + 1)
+#define STATUS_DATE_YY_MM_SEPARATOR_POS (STATUS_DATE_YY2_POS + 1)
+#define STATUS_DATE_MM1_POS (STATUS_DATE_YY_MM_SEPARATOR_POS + 1)
+#define STATUS_DATE_MM2_POS (STATUS_DATE_MM1_POS + 1)
+#define STATUS_DATE_MM_DD_SEPARATOR_POS (STATUS_DATE_MM2_POS + 1)
+#define STATUS_DATE_DD1_POS (STATUS_DATE_MM_DD_SEPARATOR_POS + 1)
+#define STATUS_DATE_DD2_POS (STATUS_DATE_DD1_POS + 1)
+#define STATUS_DATE_TIME_SEPARATOR_POS (STATUS_DATE_DD2_POS + 1)
+#define STATUS_TIME_HH1_POS (STATUS_DATE_TIME_SEPARATOR_POS + 1)
+#define STATUS_TIME_HH2_POS (STATUS_TIME_HH1_POS + 1)
+#define STATUS_TIME_HH_MM_SEPARATOR_POS (STATUS_TIME_HH2_POS + 1)
+#define STATUS_TIME_MM1_POS (STATUS_TIME_HH_MM_SEPARATOR_POS + 1)
+#define STATUS_TIME_MM2_POS (STATUS_TIME_MM1_POS + 1)
+
 
 static void uiDateAndTimeScreenEnterNextDigit(void);
 static void uiDateAndTimeScreenTimeChanged(const struct Event *event);
@@ -56,7 +72,7 @@ void uiDateAndTimeEntryScreen(void)
 		DATE_AND_TIME_ENTRY_SCREEN_LINES,
 		sizeof(uiState.screen));
 
-	uiState.input.cursorPosition = DATE_YY1_POS;
+	uiState.input.cursorPosition = ENTRY_DATE_YY1_POS;
 	uiState.input.menu.range.min = '2';
 	uiState.input.menu.range.max = '9';
 	uiState.input.buttons = &uiInputIsRange;
@@ -72,18 +88,18 @@ static void uiDateAndTimeScreenEnterNextDigit(void)
 	uiState.input.menu.range.min = '0';
 	switch (++uiState.input.cursorPosition)
 	{
-		case DATE_YY2_POS:
+		case ENTRY_DATE_YY2_POS:
 			uiState.input.menu.range.max = '9';
 			break;
 
-		case DATE_YY_MM_SEPARATOR_POS:
+		case ENTRY_DATE_YY_MM_SEPARATOR_POS:
 			uiState.input.cursorPosition++;
-			dateAndTime.date.year = uiScreenTwoDigitsFromPosition(DATE_YY1_POS);
+			dateAndTime.date.year = uiScreenTwoDigitsFromPosition(ENTRY_DATE_YY1_POS);
 			uiState.input.menu.range.max = '1';
 			break;
 
-		case DATE_MM2_POS:
-			if (uiState.screen[DATE_MM1_POS] == '1')
+		case ENTRY_DATE_MM2_POS:
+			if (uiState.screen[ENTRY_DATE_MM1_POS] == '1')
 			{
 				uiState.input.menu.range.max = '2';
 			}
@@ -94,22 +110,22 @@ static void uiDateAndTimeScreenEnterNextDigit(void)
 			}
 			break;
 
-		case DATE_MM_DD_SEPARATOR_POS:
+		case ENTRY_DATE_MM_DD_SEPARATOR_POS:
 			uiState.input.cursorPosition++;
-			dateAndTime.date.month = uiScreenTwoDigitsFromPosition(DATE_MM1_POS);
+			dateAndTime.date.month = uiScreenTwoDigitsFromPosition(ENTRY_DATE_MM1_POS);
 			if (dateAndTime.date.month == 2)
 				uiState.input.menu.range.max = '2';
 			else
 				uiState.input.menu.range.max = '3';
 			break;
 
-		case DATE_DD2_POS:
-			if (uiState.screen[DATE_DD1_POS] == '0')
+		case ENTRY_DATE_DD2_POS:
+			if (uiState.screen[ENTRY_DATE_DD1_POS] == '0')
 			{
 				uiState.input.menu.range.min = '1';
 				uiState.input.menu.range.max = '9';
 			}
-			else if (uiState.screen[DATE_DD1_POS] == uiState.input.menu.range.max)
+			else if (uiState.screen[ENTRY_DATE_DD1_POS] == uiState.input.menu.range.max)
 			{
 				uint8_t daysInMonth = clockDaysInMonth(dateAndTime.date.month, dateAndTime.date.year);
 				if (daysInMonth == 28)
@@ -123,31 +139,31 @@ static void uiDateAndTimeScreenEnterNextDigit(void)
 			}
 			break;
 
-		case DATE_TIME_SEPARATOR_POS:
+		case ENTRY_DATE_TIME_SEPARATOR_POS:
 			uiState.input.cursorPosition++;
-			dateAndTime.date.day = uiScreenTwoDigitsFromPosition(DATE_DD1_POS);
+			dateAndTime.date.day = uiScreenTwoDigitsFromPosition(ENTRY_DATE_DD1_POS);
 			uiState.input.menu.range.max = '2';
 			break;
 
-		case TIME_HH2_POS:
-			if (uiState.screen[TIME_HH1_POS] == '2')
+		case ENTRY_TIME_HH2_POS:
+			if (uiState.screen[ENTRY_TIME_HH1_POS] == '2')
 				uiState.input.menu.range.max = '3';
 			else
 				uiState.input.menu.range.max = '9';
 			break;
 
-		case TIME_HH_MM_SEPARATOR_POS:
+		case ENTRY_TIME_HH_MM_SEPARATOR_POS:
 			uiState.input.cursorPosition++;
-			dateAndTime.time.hour = uiScreenTwoDigitsFromPosition(TIME_HH1_POS);
+			dateAndTime.time.hour = uiScreenTwoDigitsFromPosition(ENTRY_TIME_HH1_POS);
 			uiState.input.menu.range.max = '5';
 			break;
 
-		case TIME_MM2_POS:
+		case ENTRY_TIME_MM2_POS:
 			uiState.input.menu.range.max = '9';
 			break;
 
-		case DATE_ENTRY_COMPLETED:
-			dateAndTime.time.minute = uiScreenTwoDigitsFromPosition(TIME_MM1_POS);
+		case ENTRY_DATE_ENTRY_COMPLETED:
+			dateAndTime.time.minute = uiScreenTwoDigitsFromPosition(ENTRY_TIME_MM1_POS);
 			// TODO: THIS NEEDS TO BE LOCAL TIME, NOT GMT...BUT IT'LL DO FOR NOW...
 			clockSetNowGmt(&dateAndTime);
 			if (uiState.flags.bits.isInitialSetupRequired)
@@ -187,12 +203,12 @@ static void uiDateAndTimeScreenTimeChanged(const struct Event *event)
 	// TODO: THIS NEEDS TO BE LOCAL TIME, NOT GMT...BUT IT'LL DO FOR NOW...
 	clockGetNowGmt(&now);
 
-	uiScreenTwoDigitsToPosition(DATE_YY1_POS, now.date.year);
-	uiScreenTwoDigitsToPosition(DATE_MM1_POS, now.date.month);
-	uiScreenTwoDigitsToPosition(DATE_DD1_POS, now.date.day);
+	uiScreenTwoDigitsToPosition(STATUS_DATE_YY1_POS, now.date.year);
+	uiScreenTwoDigitsToPosition(STATUS_DATE_MM1_POS, now.date.month);
+	uiScreenTwoDigitsToPosition(STATUS_DATE_DD1_POS, now.date.day);
 
-	uiScreenTwoDigitsToPosition(TIME_HH1_POS, now.time.hour);
-	uiScreenTwoDigitsToPosition(TIME_MM1_POS, now.time.minute);
+	uiScreenTwoDigitsToPosition(STATUS_TIME_HH1_POS, now.time.hour);
+	uiScreenTwoDigitsToPosition(STATUS_TIME_MM1_POS, now.time.minute);
 }
 
 static void uiDateAndTimeScreenStatusExit(void)
